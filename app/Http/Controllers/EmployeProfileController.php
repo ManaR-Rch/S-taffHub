2f4484eb0bf5a200407ae2459f23cd8d74d213c9
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-10 09:05:47 +0100

    add employe profile controller

---
+++ app/Http/Controllers/EmployeProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Personnel;
+use Illuminate\Http\Request;
+
+class EmployeProfileController extends Controller
+{
+    /**
+     * Afficher le profil de l'employé
+     *
+     * @return \Illuminate\View\View
+     */
+ 
+} 
