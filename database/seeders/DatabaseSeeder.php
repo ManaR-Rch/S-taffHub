d1e82a89a6ebf2e7c7f05db3b5bab69ca0b02c25
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-15 09:18:39 +0100

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,5 +8,29 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-        
+     /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       
+
+        $rh = User::create([
+            'nom' => 'Admin RH',
+            'email' => 'manarmarchou6@gmail.com',
+            'password' => Hash::make('password123'),
+            'role' => 'rh',
+        ]);
+
+
+
+        $employe = User::create([
+            'nom' => 'Employé Test',
+            'email' => 'employe@staffhub.com',
+            'password' => Hash::make('password123'),
+            'role' => 'employe',
+        ]);
+
+       
+    }    
 }
