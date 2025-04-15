bc6af316c2bc5f9b19616e2e88139df193be2b7f
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-15 09:15:53 +0100

    modify middleware isRh

--- app/Http/Controllers/EmployeProfileController.php
+++ app/Http/Controllers/EmployeProfileController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class EmployeProfileController extends Controller
 {
-
+   /**
+     * Afficher le profil de l'employé
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show()
+    {
+        $employe = Personnel::where('email', auth()->user()->email)->firstOrFail();
+        return view('employe.profile', compact('employe'));
+    }
 } 
