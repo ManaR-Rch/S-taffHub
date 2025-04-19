acca08deeb7e3ad1d2834df76f1ca4d10e0c4bab
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-19 09:33:16 +0100

    add middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployeController;
+use App\Http\Controllers\PersonnelController;
+use App\Http\Controllers\EmployeProfileController;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Middleware\IsRH;
 
 
+Route::aliasMiddleware('is_rh', IsRH::class);
 
 
-  
+Route::get('/', function () {
+  if (auth()->check()) {
+      return redirect()->intended(
+          auth()->user()->isRH() ? '/dashboard/rh' : '/dashboard/employe'
+      );
+  }
+  return view('landing');
+})->name('landing');
