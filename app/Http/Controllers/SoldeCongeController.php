5594eeef1db6d0a7097c70580c4693136a51ff83
ManaR-Rch <manarmarchou6@gmail.com>
2025-04-24 10:54:49 +0100

    add show functionand adminIndex function in  soldeCongeController

--- app/Http/Controllers/SoldeCongeController.php
+++ app/Http/Controllers/SoldeCongeController.php
@@ -8,5 +8,20 @@ use Illuminate\Http\Request;
 
 class SoldeCongeController extends Controller
 {
+    public function show()
+    {
+        $solde = auth()->user()->soldeConge;
+        return view('conges.solde', compact('solde'));
+    }
+
+    public function adminIndex()
+    {
+        if (!auth()->user()->isRH()) {
+            return redirect()->route('dashboard.employe')->with('error', 'Accès non autorisé.');
+        }
+
+        $soldes = SoldeConge::with('user')->get();
+        return view('conges.admin-solde', compact('soldes'));
+    }
 
 } 
